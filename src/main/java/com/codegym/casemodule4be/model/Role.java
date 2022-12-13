6a0228aa5e9e4769cb3d7988cee4ae513446d4b3
TranPhong04 <phongy3ulinh@gmail.com>
2022-12-13 14:43:52 +0700

    done

---
+++ src/main/java/com/codegym/casemodule4be/model/Role.java
@@ -0,0 +1,39 @@
+package com.codegym.casemodule4be.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+@Data
+public class Role implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
