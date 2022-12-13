9d13f7ecd970f9edd586e4ddfb52424165c1375e
Sonhr1403 <sonhr1403@gmail.com>
2022-12-13 10:23:55 +0700

    Initial commit

---
+++ src/test/java/com/codegym/casemodule4be/Casemodule4BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.casemodule4be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Casemodule4BeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
